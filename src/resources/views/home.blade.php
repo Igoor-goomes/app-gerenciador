54f44ce3e8e2c1623302e4817fffc56f2b92056d
Igor Gomes <igorgomesdebrito.dev@gmail.com>
2025-10-27 00:09:09 -0300

    Refactor - Ajuste na nav-bar da tela principal

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -10,13 +10,13 @@
     <div class="collapse navbar-collapse" id="navbarMain">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Sobre</a>
+          <a class="nav-link" aria-current="page" href="#">Serviços</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Perfil</a>
         </li>
       </ul>
       <form method="POST" action="{{ route('logout') }}">
